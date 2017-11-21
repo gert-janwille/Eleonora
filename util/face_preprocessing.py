c61712852bfbd2e742d9859b88722f7a69635b00
Gert-Jan Wille <gertjanwille.w@gmail.com>
2017-11-21 14:03:34 +0100

    merge conflicts removed

--- util/face_preprocessing.py
+++ util/face_preprocessing.py
@@ -3,7 +3,6 @@ This script will preprocess the facial images to train
 a neural network.
 """
 
-<<<<<<< HEAD
 import os
 import cv2
 import numpy as np
@@ -14,14 +13,6 @@ foundFace = 0
 def facechop(image, outPath):
     global foundFace
 
-=======
-import cv2
-import os
-
-FACE_CASCADE_FILE = os.path.join('../eleonora/data/haarcascades/','haarcascade_frontalface_alt.xml')
-
-def facechop(image, outPath):
->>>>>>> dev
     ar = image.split('/')
     fileName = ar[len(ar)-1].split('.')[0]
 
@@ -30,19 +21,12 @@ def facechop(image, outPath):
     img = cv2.imread(image)
 
     frame = cv2.flip(img, 1)
-<<<<<<< HEAD
+
     minisize = (200, 200)
     img_scaled = cv2.resize(frame, minisize, fx=0.25, fy=0.25, interpolation = cv2.INTER_LINEAR)
 
-
-    # miniframe = cv2.resize(img_scaled, minisize)
     gray = cv2.cvtColor(img_scaled, cv2.COLOR_BGR2GRAY)
-=======
 
-    minisize = (frame.shape[1],frame.shape[0])
-    miniframe = cv2.resize(frame, minisize)
-    gray = cv2.cvtColor(miniframe, cv2.COLOR_BGR2GRAY)
->>>>>>> dev
 
     faces = cascade.detectMultiScale(gray)
 
@@ -52,15 +36,9 @@ def facechop(image, outPath):
 
         print("Face Found:", fileName, "\n")
         sub_face = gray[y:y+h, x:x+w]
-<<<<<<< HEAD
         face_file_name = outPath + "_processed/" + fileName + ".jpg"
         cv2.imwrite(face_file_name, sub_face)
         foundFace = foundFace + 1
-=======
-        face_file_name = outPath + fileName + ".jpg"
-        os.remove(image)
-        cv2.imwrite(face_file_name, sub_face)
->>>>>>> dev
 
 
 if __name__ == '__main__':
@@ -81,8 +59,4 @@ if __name__ == '__main__':
         facechop(os.path.join(path,f), path)
 
 
-<<<<<<< HEAD
     print("Found Faces: %s/%s " % (foundFace, imageCounter))
-=======
-    print("Found Images:", imageCounter)
->>>>>>> dev
